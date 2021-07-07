3459b8a3549ee42ffee395fd6fe35a74cce0b2a0
worldrules <worldrulesdev@gmail.com>
2021-07-07 01:07:39 -0300

    kk

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Accordion from './components/Accordion';
+
+
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title:'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by Creating Components'
+    }
+]
+
+const App = () => {
+    return (
+        <div className="content">
+            <Accordion items={items}></Accordion>
+        </div>
+    )
+    
+};
+
+export default App;
