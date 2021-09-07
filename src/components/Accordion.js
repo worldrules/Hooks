9e1059381adb43dc6a61264b2cfc24620d97dbb5
worldrules <leonardo.carvalho@acertsis.com.br>
2021-09-07 18:30:21 -0300

    useState added

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
 
     const onTitleClick = (index) => {
-        console.log('title clicked', index)
+        setActiveIndex(index);
     }
 
     const renderedItems = items.map((item, index) => {
@@ -27,6 +28,7 @@ const Accordion = ({ items }) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
+        <h1>{activeIndex}</h1>
     </div>
 }
 
